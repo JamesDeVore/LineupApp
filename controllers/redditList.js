f370c7079ec9829472c7a6512ff2ee87952ea582
James DeVore <jdevore4592@gmail.com>
2019-02-20 11:22:31 -0500

    new API added

--- controllers/redditList.js
+++ controllers/redditList.js
@@ -1,51 +1,36 @@
+const moment = require('moment')
+
+
 let fetch = require('node-fetch')
 exports.getListings = async (req, res) => {
-  allPosts = [];
-  let terms = ["Lineup", "lineup", "line up", "Line up", "setlist", "SetList", "setList","set list", "theme", "when will", "LINEUP"]
-
-  let response = await fetch('https://www.reddit.com/r/electricdaisycarnival/.json?restrict_sr=on&t=all&limit=100&').then(r => r.json());
+  let response = await fetch('https://api.pushshift.io/reddit/search/submission/?q=lineup&subreddit=electricdaisycarnival&size=500').then(r => r.json());
   let{data} = response;
-  let lineupPosts = data.children.filter(posts => {
-    let foundOne = false;
-    let {data:{title,selftext}} = posts;
-    terms.forEach(term => {
-      if(selftext.includes(term) || title.includes(term)){
-        foundOne = true;
-      }
-    })
-    return foundOne
-  })
-  allPosts = allPosts.concat(lineupPosts)
-  while(data.after){
-    let{after} = data
-    response = await fetch(`https://www.reddit.com/r/electricdaisycarnival/.json?restrict_sr=on&t=all&limit=100&after=${after}`).then(r => r.json());
-    let moreLineupPosts = data.children.filter(posts => {
-      let foundOne = false;
-      let { data: { title, selftext } } = posts;
-      terms.forEach(term => {
-        if (selftext.includes(term) || title.includes(term)) {
-          foundOne = true;
-        }
-      })
-      return foundOne
-    })
-    allPosts = allPosts.concat(moreLineupPosts)
-    data = response.data
-  }
+  // while(data.after){
+  //   console.log(data.after)
+  //   let{after} = data
+  //   response = await fetch(`https://www.reddit.com/r/electricdaisycarnival/.json?limit=100&t=all&after=${after}`).then(r => r.json());
+  //   let moreLineupPosts = data.children.filter(posts => {
+  //     let foundOne = false;
+  //     let { data: { title, selftext } } = posts;
+  //     terms.forEach(term => {
+  //       if (selftext.includes(term) || title.includes(term)) {
+  //         foundOne = true;
+  //       }
+  //     })
+  //     return foundOne
+  //   })
+  //   allPosts = allPosts.concat(moreLineupPosts)
+  //   // console.log(response.data)
+  //   data = response.data
+    
+  // }
   let sendObj = {
-    totalPosts:allPosts.length,
-    allPosts
+    totalPosts:data.length,
+    data
   }
+  data.forEach(post => {
+    console.log(moment.unix(post.created_utc).format("MM/DD/YYYY"))
+  })
   res.send(sendObj)
 }
 
-const _filterFunction = posts => {
-    let { data:{selftext,title} } = posts;
-    let searchTerms = ["Lineup", "lineup", "Line up", "line up"]
-    searchTerms.forEach(term => {
-      if(selftext.includes(term) || title.includes(term)){
-        return true;
-      }
-    })
-    return false
-}
